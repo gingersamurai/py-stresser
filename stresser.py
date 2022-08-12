0f20ba08be8bd629d07b3a666fdc1189f918e5e2
gingersamurai <nazimmalyshev47@gmail.com>
2022-08-12 13:44:10 +0300

    changed path for tests

--- stresser.py
+++ stresser.py
@@ -1,30 +1,54 @@
+"""
+Основной скрипт для запуска тестов.
+настраивается с помощью config.py
+"""
+
+
+
+
+
+# для работы с ОС
 import os
+
+# конфиг
 from stresser_data.config import *
+
+# доп модули
 from stresser_data.modules import *
 
+# в stresser_data создаем папку tests с тестами
+# и files с испольяемыми файлами решений
 if not os.path.exists(f"stresser_data{DLM}files"):
     os.mkdir(f"stresser_data{DLM}files")
 if not os.path.exists(f"stresser_data{DLM}tests"):
     os.mkdir(f"stresser_data{DLM}tests")
 
+# очищаем папки от старых данных
 clean(tests=True, files=True)
 
+# копмилируем решения
 launch_solution = compile(SOLUTION_PATH, "solution")
 launch_dummy = compile(DUMMY_PATH, "dummy")
 
+# в цикле перебираем тесты и запускаем
 for test_num in range(1, TEST_CNT):
     
-    test_path = f"stresser_data{DLM}tests{DLM}in_{test_num}_.txt"
-    dummy_out_path = f"stresser_data{DLM}tests{DLM}dummy_out_{test_num}_.txt"
-    solution_out_path = f"stresser_data{DLM}tests{DLM}solution_out_{test_num}_.txt"
+    # прописываем пути для тестов
+    test_path = f"stresser_data{DLM}tests{DLM}{test_num}_in_.txt"
+    dummy_out_path = f"stresser_data{DLM}tests{DLM}{test_num}_dummy_out_.txt"
+    solution_out_path = f"stresser_data{DLM}tests{DLM}{test_num}_solution_out_.txt"
     
+    #создаем файл и записываем в него сгенерирорванный тест
     test_now = open(test_path, "a")
     test_now.write(GEN_TEST(test_num))
     test_now.close()
 
+    # запускаем наши решения, на вход даем тест
+    # выходные данные сохраняем в отдельных файлах
     launch_dummy(test_path, dummy_out_path)
     launch_solution(test_path, solution_out_path)
 
+    # считываем выходные данные
     f_dummy_out = open(dummy_out_path, "r")
     dummy_out = f_dummy_out.read()
     f_dummy_out.close()
@@ -33,9 +57,11 @@ for test_num in range(1, TEST_CNT):
     solution_out = f_solution_out.read()
     f_solution_out.close()
     
+    # если пользователь не просит сохранять тесты, удаляем
     if not SAVE_TESTS:
         clean(tests=True)
 
+    # проверяем с помощью чекера
     if CHECK_TEST(dummy_out, solution_out) == True:
         print(f"test {test_num} OK")
     else:
