133c77254cde6e827d1e0d9cdafee71afcb9f77c
gingersamurai <nazimmalyshev47@gmail.com>
2022-09-03 23:15:24 +0300

    added default values in parse.py

--- modules/parse.py
+++ modules/parse.py
@@ -6,13 +6,28 @@ CHECKER_TYPES = ['base', 'base_with_format']
 
 def validate(in_data: dict):
     for now_path in ['SOLUTION_PATH', 'DUMMY_PATH', 'GENERATOR_PATH']:
-        assert (now_path in in_data and in_data[now_path]) != None, f"there is not {now_path}"
-        if not os.path.isabs(in_data[now_path]):
-            in_data[now_path] = os.path.abspath(in_data[now_path])
-        assert os.path.isfile(in_data[now_path]), f"{in_data[now_path]} not such file"
-    assert ('NTESTS' in in_data) and (type(in_data["NTESTS"]) == int) and (in_data["NTESTS"] > 0), "NTESTS must be positive integer"
-    assert ('SAVE_TESTS' in in_data), "must be SAVE_TESTS (true/false)"
-    assert ('CHECKER_TYPE' in in_data) and (in_data['CHECKER_TYPE'] in CHECKER_TYPES), "wrong CHECKER_TYPE"
+        assert (in_data[now_path] != None), f"there is not {now_path}"
+        # if not os.path.isabs(in_data[now_path]):
+        #     in_data[now_path] = os.path.abspath(in_data[now_path])
+        assert os.path.isfile(in_data[now_path]), in_data[now_path] + ': not such file'
+    
+    assert (type(in_data["NTESTS"]) == int) and (in_data["NTESTS"] > 0), "NTESTS must be positive integer"
+    assert (in_data['CHECKER_TYPE'] in CHECKER_TYPES), "wrong CHECKER_TYPE"
+
+def set_default(in_data: dict):
+    def_dict = {
+        'NTESTS': 10000,
+        'SAVE_TESTS': False,
+        'CHECKER_TYPE': 'base_with_format',
+        'SOLUTION_PATH': None,
+        'DUMMY_PATH': None,
+        'GENERATOR_PATH': None
+    }
+    for key, val in in_data.items():
+        if val != None:
+            def_dict[key] = val
+    
+    return def_dict
 
 
 def parse_input() -> dict:
@@ -23,11 +38,10 @@ def parse_input() -> dict:
     parser.add_argument("-S", "--SOLUTION_PATH", help="путь к решению с ошибками")
     parser.add_argument("-D", "--DUMMY_PATH", help="путь к правильному решению")
     parser.add_argument("-G", "--GENERATOR_PATH", help="путь к программе, которая генерирует данные в stdout")
-    parser.add_argument("-N", "--NTESTS", type=int, help="необходимое количество тестов")
-    parser.add_argument("-s", "--SAVE_TESTS", action="store_true", help="если прописан, то \
-    все пройденные тесты будут сохраняться")
+    parser.add_argument("-N", "--NTESTS", type=int, help="необходимое количество тестов. Базовое значение: 10000")
+    parser.add_argument("-s", "--SAVE_TESTS", help="сохранять ли тесты. варианты: true/false")
     parser.add_argument("-t", "--CHECKER_TYPE", help=f"тип чекера.\
-    варианты: {CHECKER_TYPES}")
+    варианты: {CHECKER_TYPES} Базовое значение: base_with_format")
     args = parser.parse_args()
     res_dict = {}
     if args.CONFIG_PATH == None:
@@ -40,5 +54,6 @@ def parse_input() -> dict:
         res_dict = (dict(config['settings']))
         res_dict['NTESTS'] = config.getint('settings', 'NTESTS')
         res_dict['SAVE_TESTS'] = config.getboolean('settings', 'SAVE_TESTS')
+    res_dict = set_default(res_dict)
     validate(res_dict)
     return res_dict
